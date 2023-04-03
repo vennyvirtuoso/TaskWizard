38bc7e8ec8e23834860a91adaa9fba674a0851f0
vennyvirtuoso <vijendra9753@gmail.com>
2023-04-04 02:41:15 +0530

    Initial Commit

---
+++ TaskWizard/TaskWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskWizardApp.swift
+//  TaskWizard
+//
+//  Created by Vijendra Vaishya on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
